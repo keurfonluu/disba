9637218f1cd0239e3f902b505f2b13a7f658b1b5
Keurfon Luu <keurfonluu@outlook.com>
2020-06-20 20:16:56 -0700

    add figures

---
+++ .github/perf.py
@@ -0,0 +1,59 @@
+import numpy
+import perfplot
+
+from disba import PhaseDispersion, srfdis
+from pysurf96 import surf96
+
+
+def velocity_model(n):
+    thickness = numpy.full(n, 0.5)
+    velocity_p = 1.0 + 0.1 * numpy.arange(n)
+    velocity_s = velocity_p / 1.74
+    density = numpy.full(n, 2.0)
+
+    return thickness, velocity_p, velocity_s, density
+
+
+def perf_disba(n, periods, wave, algorithm="dunkin"):
+    pd = PhaseDispersion(*velocity_model(n), algorithm=algorithm)
+    return pd(periods, wave=wave).velocity
+
+
+def perf_pysurf96(n, periods, wave):
+    return surf96(*velocity_model(n), periods, wave, 1, "phase")
+
+
+config = {
+    "rayleigh": {
+        "kernels": [
+            lambda n: perf_pysurf96(n, periods, "rayleigh"),
+            lambda n: perf_disba(n, periods, "rayleigh"),
+            lambda n: perf_disba(n, periods, "rayleigh", "fast-delta"),
+        ],
+        "labels": ["pysurf96", "disba-dunkin", "disba-fast-delta"],
+        "title": "Rayleigh-wave",
+        "filename": "perf_rayleigh.png",
+    },
+    "love": {
+        "kernels": [
+            lambda n: perf_pysurf96(n, periods, "love"),
+            lambda n: perf_disba(n, periods, "love"),
+        ],
+        "labels": ["pysurf96", "disba"],
+        "title": "Love-wave",
+        "filename": "perf_love.png",
+    },
+}
+periods = numpy.logspace(0.0, 1.0, 60)
+
+for wave in config.values():
+    out = perfplot.bench(
+        setup=lambda n: n,
+        n_range=numpy.arange(19) + 2,
+        kernels=wave["kernels"],
+        equality_check=lambda a, b: numpy.allclose(a, b, atol=0.02),
+        title=wave["title"],
+        xlabel="Number of layers",
+        labels=wave["labels"],
+    )
+    out.save(wave["filename"], transparent=True, bbox_inches="tight")
---
+++ .github/sample.py
@@ -0,0 +1,33 @@
+import numpy
+
+from disba import PhaseDispersion
+
+import dufte
+import matplotlib.pyplot as plt
+
+plt.style.use(dufte.style)
+
+
+velocity_model = numpy.array([
+    [0.5, 1.0, 0.5, 1.8],
+    [0.3, 2.0, 1.0, 1.8],
+    [10.0, 1.0, 0.5, 1.8],
+])
+pd = PhaseDispersion(*velocity_model.T, algorithm="fast-delta")
+f = numpy.linspace(0.1, 10.0, 100)
+t = 1.0 / f[::-1]
+
+for wave in ["rayleigh", "love"]:
+    fig = plt.figure()
+
+    for i in range(20):
+        cp = pd(t, mode=i, wave=wave, dc=0.001)
+        plt.plot(1.0 / cp.period, cp.velocity, color="#1f77b4", linewidth=1)
+
+    plt.title(f"{wave.capitalize()}-wave")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Phase velocity [km/s]")
+    plt.xlim(0.0, 10.0)
+    plt.ylim(0.45, 1.0)
+
+    fig.savefig(f"sample_{wave}.png", dpi=100, transparent=True, bbox_inches="tight")
