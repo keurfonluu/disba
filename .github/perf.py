d988d1d348247e147a602cf4a38d4370549c50fc
Keurfon Luu <keurfonluu@outlook.com>
2020-06-22 00:06:25 -0700

    update velocity_model

--- .github/perf.py
+++ .github/perf.py
@@ -8,7 +8,7 @@ from pysurf96 import surf96
 def velocity_model(n):
     thickness = numpy.full(n, 0.5)
     velocity_p = 1.0 + 0.1 * numpy.arange(n)
-    velocity_s = velocity_p / 1.74
+    velocity_s = velocity_p / 1.73
     density = numpy.full(n, 2.0)
 
     return thickness, velocity_p, velocity_s, density
