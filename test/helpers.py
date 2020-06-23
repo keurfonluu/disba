d799f6351e4f0033324d846814ab2c870aebe1a5
Keurfon Luu <keurfonluu@outlook.com>
2020-06-23 01:02:00 -0700

    test water layer

--- test/helpers.py
+++ test/helpers.py
@@ -1,10 +1,14 @@
 import numpy
 
 
-def velocity_model(n):
+def velocity_model(n, water_layer=False):
     thickness = numpy.full(n, 0.5)
     velocity_p = 1.0 + 0.1 * numpy.arange(n)
     velocity_s = velocity_p / 1.73
     density = numpy.full(n, 2.0)
+
+    if water_layer:
+        velocity_s[0] = 0.0
+        density[0] = 1.0
 
     return thickness, velocity_p, velocity_s, density
