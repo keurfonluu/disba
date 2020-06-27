c22f104419c15f92a2ce9529fdcfd6bab384bc23
Keurfon Luu <keurfonluu@outlook.com>
2020-06-27 00:34:53 -0700

    update sample

--- .github/sample.py
+++ .github/sample.py
@@ -1,6 +1,6 @@
 import numpy
 
-from disba import PhaseDispersion
+from disba import EigenFunction, PhaseDispersion, PhaseSensitivity
 
 import dufte
 import matplotlib.pyplot as plt
@@ -8,26 +8,98 @@ import matplotlib.pyplot as plt
 plt.style.use(dufte.style)
 
 
-velocity_model = numpy.array([
-    [0.5, 1.0, 0.5, 1.8],
-    [0.3, 2.0, 1.0, 1.8],
-    [10.0, 1.0, 0.5, 1.8],
-])
-pd = PhaseDispersion(*velocity_model.T, algorithm="fast-delta", dc=0.001)
-f = numpy.linspace(0.1, 10.0, 100)
-t = 1.0 / f[::-1]
+# Velocity model and periods
+velocity_model = numpy.array(
+    [
+        [10.0, 7.00, 3.50, 2.00],
+        [10.0, 6.80, 3.40, 2.00],
+        [10.0, 7.00, 3.50, 2.00],
+        [10.0, 7.60, 3.80, 2.00],
+        [10.0, 8.40, 4.20, 2.00],
+        [10.0, 9.00, 4.50, 2.00],
+        [10.0, 9.40, 4.70, 2.00],
+        [10.0, 9.60, 4.80, 2.00],
+        [10.0, 9.50, 4.75, 2.00],
+    ]
+)
+t = numpy.logspace(0.0, 3.0, 100)
 
+
+# Dispersion curve
+pd = PhaseDispersion(*velocity_model.T)
+labels = {
+    0: "Fundamental",
+    1: "Mode 1",
+    2: "Mode 2",
+}
 for wave in ["rayleigh", "love"]:
     fig = plt.figure()
 
-    for i in range(20):
+    for i in range(3):
         cp = pd(t, mode=i, wave=wave)
-        plt.plot(1.0 / cp.period, cp.velocity, color="#1f77b4", linewidth=1)
+        plt.semilogx(cp.period, cp.velocity, linewidth=1, label=labels[i])
 
     plt.title(f"{wave.capitalize()}-wave")
-    plt.xlabel("Frequency [Hz]")
+    plt.xlabel("Period [s]")
     plt.ylabel("Phase velocity [km/s]")
-    plt.xlim(0.0, 10.0)
-    plt.ylim(0.45, 1.0)
+    plt.ylim(3.2, 4.8)
+    plt.legend(loc=4, frameon=False)
 
     fig.savefig(f"sample_{wave}.svg", transparent=True, bbox_inches="tight")
+
+
+# Resample velocity model with respect to depth
+thickess = numpy.ones(161) * 0.5
+idx = numpy.digitize(thickess.cumsum(), velocity_model[:, 0].cumsum(), right=True)
+velocity_model = velocity_model[idx]
+velocity_model[:, 0] = thickess
+
+
+# Eigenfunction
+eigf = EigenFunction(*velocity_model.T)
+keys = {
+    "rayleigh": ["ur", "uz", "tz", "tr"],
+    "love": ["uu", "tt"],
+}
+for wave in ["rayleigh", "love"]:
+    fig = plt.figure()
+
+    eig = eigf(20.0, mode=0, wave=wave)
+    for key in keys[wave]:
+        plt.plot(getattr(eig, key), eig.depth, linewidth=1, label=key.upper())
+
+    plt.title(f"{wave.capitalize()}-wave")
+    plt.xlabel("Normalized eigenfunction")
+    plt.ylabel("Depth [km]")
+    plt.xlim(-2.0, 2.0)
+    plt.ylim(0.0, 80.0)
+    plt.gca().invert_yaxis()
+    plt.legend(loc=4, frameon=False)
+
+    fig.savefig(f"eigen_{wave}.svg", transparent=True, bbox_inches="tight")
+
+
+# Sensitivity kernel
+ps = PhaseSensitivity(*velocity_model.T, dp=0.025)
+labels = {
+    "thickness": "$\\partial c / \\partial d$",
+    "velocity_p": "$\\partial c / \\partial \\alpha$",
+    "velocity_s": "$\\partial c / \\partial \\beta$",
+    "density": "$\\partial c / \\partial \\rho$",
+}
+for wave in ["rayleigh", "love"]:
+    fig = plt.figure()
+
+    for parameter in ["thickness", "velocity_p", "velocity_s", "density"]:
+        sk = ps(20.0, mode=0, wave=wave, parameter=parameter)
+        plt.plot(sk.kernel, sk.depth, linewidth=1, label=labels[parameter])
+
+    plt.title(f"{wave.capitalize()}-wave")
+    plt.xlabel("Sensitivity kernel")
+    plt.ylabel("Depth [km]")
+    plt.xlim(-0.04, 0.04)
+    plt.ylim(0.0, 80.0)
+    plt.gca().invert_yaxis()
+    plt.legend(loc=4, frameon=False)
+
+    fig.savefig(f"kernel_{wave}.svg", transparent=True, bbox_inches="tight")
