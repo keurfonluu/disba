3efa0a186d4a70d32c03e7d9188bf5017c6ec54b
Keurfon Luu <keurfonluu@outlook.com>
2020-06-26 21:50:07 -0700

    implement eigenfunction

--- disba/__init__.py
+++ disba/__init__.py
@@ -1,15 +1,23 @@
 from .__about__ import __version__
+
 from ._dispersion import DispersionCurve, GroupDispersion, PhaseDispersion
+from ._eigen import LoveEigen, RayleighEigen, EigenFunction
 from ._sensitivity import GroupSensitivity, PhaseSensitivity, SensitivityKernel
+
 from ._surf96 import surf96
+from ._swegn96 import swegn96
 
 __all__ = [
     "surf96",
+    "swegn96",
     "DispersionCurve",
     "PhaseDispersion",
     "GroupDispersion",
     "SensitivityKernel",
     "PhaseSensitivity",
     "GroupSensitivity",
+    "LoveEigen",
+    "RayleighEigen",
+    "EigenFunction",
     "__version__",
 ]
