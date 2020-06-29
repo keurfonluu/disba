9e25a970162715d93d8a1f4b9ee652100f3c0f60
Keurfon Luu <keurfonluu@outlook.com>
2020-06-29 00:42:08 -0700

    invoke format

--- disba/__init__.py
+++ disba/__init__.py
@@ -1,10 +1,8 @@
 from .__about__ import __version__
-
 from ._dispersion import DispersionCurve, GroupDispersion, PhaseDispersion
 from ._eigen import EigenFunction, LoveEigen, RayleighEigen
 from ._ellipticity import Ellipticity, RayleighEllipticity
 from ._sensitivity import GroupSensitivity, PhaseSensitivity, SensitivityKernel
-
 from ._surf96 import surf96
 from ._swegn96 import swegn96
 
