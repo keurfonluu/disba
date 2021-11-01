b8b8725f95aa31101d65cf614f06381210d4531f
Keurfon Luu <keurfonluu@outlook.com>
2021-11-01 12:25:36 -0700

    use book theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath(".../"))
 import disba
 
 project = "disba"
-copyright = "2020, Keurfon Luu"
+copyright = "2021, Keurfon Luu"
 author = "Keurfon Luu"
 
 version = disba.__version__
@@ -123,7 +123,7 @@ autoclass_content = "both"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 html_theme_path = [
     "_themes",
 ]
